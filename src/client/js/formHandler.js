5968aae9ef83c88a3a731a24c091f11bf66d1bf2
Jocker <joker.dmitri@gmail.com>
2022-03-09 13:30:12 +1100

    Client code

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,14 +3,30 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
-
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    const data = {
+        formText
+    }
+    if (Client.checkForName(formText)) {
+        fetch('http://localhost:8081/userText', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(res => res.json())
+            .then(function (res) {
+                document.getElementById('model').innerHTML = 'Model: ' + res.model;
+                document.getElementById('score_tag').innerHTML = 'Score: ' + res.score_tag;
+                document.getElementById('agreement').innerHTML = 'Agreement: ' + res.agreement;
+                document.getElementById('subjectivity').innerHTML = 'Subjectivity: ' + res.subjectivity;
+                document.getElementById('confidence').innerHTML = 'Confidence: ' + res.confidence;
+                document.getElementById('irony').innerHTML = 'Irony: ' + res.irony;
+            })
+    } else {
+        alert ('Please enter a valid URL')
+    };
 }
 
 export { handleSubmit }
