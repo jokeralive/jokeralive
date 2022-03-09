877ccfc676e2ea3a031092be77d20f8da83c2564
Jocker <joker.dmitri@gmail.com>
2022-03-09 14:31:14 +1100

    Add html plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
@@ -28,6 +29,10 @@ module.exports = {
         minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
         },
     plugins: [
+        new HtmlWebPackPlugin({
+          template: "./src/client/views/index.html",
+          filename: "./index.html",
+        }),
         new MiniCssExtractPlugin({ filename: "[name].css" }),
         new WorkboxPlugin.GenerateSW()
     ]
